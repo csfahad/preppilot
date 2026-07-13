bc785f2581dde8833238f75fb3538de724b4832b
csfahad <dev.csfahad@gmail.com>
2026-07-13 23:39:14 +0530

    chore: vite config update

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { nitro } from "nitro/vite";
-
-import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 const config = defineConfig({
     envDir: "../..",
     resolve: { tsconfigPaths: true },
-    plugins: [devtools(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
+    plugins: [tanstackStart(), tailwindcss(), devtools()],
 });
 
 export default config;
