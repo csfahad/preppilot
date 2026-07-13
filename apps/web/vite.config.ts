d971e4aae6c6c3e0c672bd72672b7e1cb1400752
csfahad <dev.csfahad@gmail.com>
2026-07-13 23:45:03 +0530

    fix: vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 const config = defineConfig({
     envDir: "../..",
     resolve: { tsconfigPaths: true },
-    plugins: [tanstackStart(), tailwindcss(), devtools()],
+    plugins: [tanstackStart(), tailwindcss(), devtools(), nitro()],
 });
 
 export default config;
